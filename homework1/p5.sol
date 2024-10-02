825b6bc000f7bd1af0287235954926ed00bf561c
Perry Wu <yswuab@connect.ust.hk>
2024-10-02 17:47:20 +0800

    hw1 p5

---
+++ homework1/p5.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+contract RentalAgreement {
+    address tenant; // Alice
+    address  payable landlord; // Bob
+    uint rentAmount;
+    uint dueDate; // Monthly due date in Unix timestamp
+    uint penaltyAmount;
+    bool isPaid;
+    constructor(address _tenant, address payable _landlord, uint _rentAmount,
+        uint _dueDate, uint _penaltyAmount) {
+        tenant = _tenant;
+        landlord = _landlord;
+        rentAmount = _rentAmount;
+        dueDate = _dueDate;
+        penaltyAmount = _penaltyAmount;
+        isPaid = false;
+    }
+
+    // Function to make rent payment
+    function payRent() public payable {
+        require(msg.sender == tenant, "Only the tenant can pay the rent.");
+        require(isPaid== false, "Only can pay once a month");
+        uint _rentAmount = rentAmount;
+        require( block.timestamp < dueDate); {
+            // Apply penalty
+            _rentAmount+=penaltyAmount;
+        }
+        // Transfer rent to landlord
+        landlord.transfer(_rentAmount);
+        isPaid = true;
+    }
+    // Reset payment status at the start of each month
+    function resetPaymentStatus() public {
+        require( isPaid==true,"isPaid must be true" );
+        isPaid = false;
+    }
+    // Function to check if rent is paid
+    function checkPaymentStatus() public view returns (bool) {
+        return isPaid;
+    }
+}
--- practice/week3/Variables/Variables.sol
+++ practice/week3/Variables/Variables.sol
@@ -5,13 +5,13 @@ contract Variables {
     // State variables are stored on the blockchain.
     string public text = "Hello";
     uint256 public num = 123;
-
+    uint256 public timestamp;
     function doSomething() public {
         // Local variables are not saved to the blockchain.
         uint256 i = 456;
 
         // Here are some global variables
-        uint256 timestamp = block.timestamp; // Current block timestamp
+        timestamp = block.timestamp; // Current block timestamp
         address sender = msg.sender; // address of the caller
     }
 }
