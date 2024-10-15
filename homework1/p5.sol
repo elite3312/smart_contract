6b98266fd41a5ec0a2024b0df59a34635df7c2a5
Perry Wu <yswuab@connect.ust.hk>
2024-10-15 17:06:08 +0800

    complete hw1

--- homework1/p5.sol
+++ homework1/p5.sol
@@ -22,7 +22,7 @@ contract RentalAgreement {
         require(msg.sender == tenant, "Only the tenant can pay the rent.");
         require(isPaid== false, "Only can pay once a month");
         uint _rentAmount = rentAmount;
-        require( block.timestamp <= dueDate); {
+        if( block.timestamp <= dueDate) {
             // Apply penalty
             _rentAmount+=penaltyAmount;
         }
@@ -33,7 +33,16 @@ contract RentalAgreement {
     // Reset payment status at the start of each month
     function resetPaymentStatus() public {
         require( isPaid==true,"isPaid must be true" );
+        require(isStartOfMonth(),"must be day 1 of month" );
         isPaid = false;
+    }
+
+    function isStartOfMonth()  view public  returns (bool) {
+        // Get the current block timestamp
+        uint256 timestamp = block.timestamp;
+        uint256 day = (timestamp / 86400) % 31 + 1; // Approximate day calculation
+        if (day==1)return true;
+        return false;
     }
     // Function to check if rent is paid
     function checkPaymentStatus() public view returns (bool) {
