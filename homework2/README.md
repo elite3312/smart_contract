f37f6f2690fd1e53e4e0072b089c2a24730c8c22
Perry Wu <yswuab@connect.ust.hk>
2024-10-22 22:01:52 +0800

    hw2 p3

--- homework2/README.md
+++ homework2/README.md
@@ -2,15 +2,24 @@
 
 ## Problem 1
 
-### state variables
-
-- the owner of the project can call finalize a project
 - voting power
   - the voting power of a person is determined by the sum of donations he made to the fund.
 - everyone can create a project
   - some variables are created to maintain the votes casted on each project
+  - projects are identified by an integer ID, starting from 0
   - a project can be finalized once 50% voting power is voted on it
     - but only the owner can call finalize()
 - logging
   - some state variables are created for logging finalized projects and donation received
 
+## Problem 2
+
+## Problem 3
+
+- struct aggreement holds details of each rental agreement.
+- we use an array to hold multiple aggreements, each can have different landlord and tenants
+  - for each aggreement, we use a state variable remaining_duration to record the remaining duration in the lease.
+- only the tenants of each aggreement can pay, and they must pay the exact amount
+- only the landlord can terminate the aggreement, and to do so the lease must be complete.
+  - otherwise, we give the owner of the contract the right to terminate an aggreement in case of violation of aggreement.
+  
---
+++ homework2/p3_rental_aggreement.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract RentalAgreementManagement {
+    struct Agreement {
+        address payable landlord;
+        address tenant;
+        uint256 rentAmount;
+        uint256 startTime;
+        uint256 duration;//in months
+        uint256 remaining_duration;//in months
+        bool active;
+    }
+
+    uint256 public agreementCount;//ctr
+    
+    address owner;//owner
+    mapping(uint256 => Agreement) public agreements;//list of agreements
+    mapping(uint256 => uint256[]) public paymentHistory;//payement history for each agreement
+
+    event AgreementCreated(uint256 agreementId, address landlord, address tenant, uint256 rentAmount, uint256 duration);
+    event RentPaid(uint256 agreementId, address tenant, uint256 amount);
+    event AgreementTerminated(uint256 agreementId, address landlord);
+    constructor() {
+        owner=msg.sender;
+        agreementCount = 0;
+    }
+
+    function createAgreement(address payable _landlord,address _tenant, uint256 _rentAmountInGwei  , uint256 _duration_months) public {
+        //creates an leasing aggreement between the landlord and the tenant
+        uint256 rentAmountInWei = _rentAmountInGwei * 1 gwei;
+        agreements[agreementCount] = Agreement({
+            landlord: _landlord,
+            tenant: _tenant,//another address
+            rentAmount: rentAmountInWei ,//monthly
+            startTime: block.timestamp,
+            duration:_duration_months,
+            remaining_duration:_duration_months,//counts down until the end of the lease
+            active: true
+        });
+        agreementCount++;
+
+        emit AgreementCreated(agreementCount, msg.sender, _tenant, rentAmountInWei, _duration_months);
+    }
+
+    function payRent(uint256 _agreementId) public payable {
+        //only the tenant can pay, and must pay in exact amount in Gwei
+        Agreement storage agreement = agreements[_agreementId];
+        require(agreement.active, "Agreement is not active");
+        require(msg.sender == agreement.tenant, "Only tenant can pay rent");
+        require(msg.value == agreement.rentAmount, "Incorrect rent amount");
+        require(agreement.remaining_duration>=1,"Remaining Duration Must Be Greater Than Or Equal to 1 Month");
+        //pay
+        agreement.landlord.transfer(msg.value);
+        paymentHistory[_agreementId].push(block.timestamp);
+        agreement.remaining_duration-=1;
+        emit RentPaid(_agreementId, msg.sender, msg.value);
+    }
+
+    function terminateAgreement(uint256 _agreementId) public {
+        Agreement storage agreement = agreements[_agreementId];
+        require(msg.sender == agreement.landlord ||msg.sender == owner, "Only landlord or contract owner can terminate");
+        require(agreement.active, "Agreement is already terminated");
+        
+
+        //must be end of lease if you are landlord
+        if (msg.sender == agreement.landlord )
+            if (!(agreement.remaining_duration==0)){
+                revert();
+        }
+        //else if you are contract owner you can still terminate the contract
+
+        agreement.active = false;
+
+        emit AgreementTerminated(_agreementId, msg.sender);
+    }
+
+    function getAgreementStatus(uint256 _agreementId) public view returns (string memory) {
+        Agreement storage agreement = agreements[_agreementId];
+        if (agreement.active) {
+            return "Active";
+        } else {
+            return "Terminated";
+        }
+    }
+}
